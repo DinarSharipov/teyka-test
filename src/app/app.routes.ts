e8de6150fc49ff3ddc93a9bf892bfc5479a77ffd
Dinar <sharipovdinar03@gmail.com>
2026-02-04 17:47:50 +0300

    редирект на пустой роут

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,11 @@ import { Login } from './pages/login/login';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
     path: 'login',
     component: Login,
     canActivate: [noAuthGuard],
